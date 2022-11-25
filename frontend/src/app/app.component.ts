4350d7157c47dcba7adeccb77a3f6350642c6a4f
Akshar <akshar.1006@gmail.com>
2022-11-25 17:38:03 -0500

    some more skeleton

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,14 +11,21 @@ export class AppComponent {
   tokenContractAddress: string | undefined;
   ballotContractAddress: string | undefined;
 
+  tokenRequestPending: boolean;
+
   constructor(private http: HttpClient) {
     this.backendUrl = "http://localhost:3000"
     this.http.get<any>(`${this.backendUrl}/get-token-contract-address`).subscribe((ans) => {
       this.tokenContractAddress = ans.result;
     })
+    this.tokenRequestPending = false;
   }
 
   connectBallotContract(address: string) {
     this.ballotContractAddress = address;
+  }
+
+  requestTokens(amount: string) {
+    this.tokenRequestPending = true;
   }
 }
